1ed2e1706d23e0d698b2dd4fc1c5803237eabc3b
Juan Arnold <juanwishnutopia@gmail.com>
2023-09-17 20:03:25 +0700

    Revised scanning method, no need to make separate folders

--- football_star_recognition.py
+++ football_star_recognition.py
@@ -3,78 +3,57 @@ import matplotlib
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-train_dir='C:/Users/admin/OneDrive - Bina Nusantara/Binus/Semester 5/Computer Vision/LAB/Project/Dataset/train'
-test_dir='C:/Users/admin/OneDrive - Bina Nusantara/Binus/Semester 5/Computer Vision/LAB/Project/Dataset/test'
+data_dir='C:/Users/admin/OneDrive - Bina Nusantara/Binus/Semester 5/Computer Vision/LAB/Project/Dataset/'
 choice=0
 
-def train_and_test(train_dir,test_dir):
-    train=[]
-    test=[]
+def train_and_test(data_dir):
+    images=[]
+    Ls=[]
     face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    train_labels=os.listdir(train_dir)
-    test_labels=os.listdir(test_dir)
+    labels=os.listdir(data_dir)
 
-    #Getting images from the train folder
-    for label in train_labels:
-        if os.path.isdir(os.path.join(train_dir,label)):
-            temporary_folder=os.path.join(train_dir,label)
-            images=os.listdir(temporary_folder)
+    #Getting images from the dataset folder
+    for label in labels:
+        if os.path.isdir(os.path.join(data_dir,label)):
+            temporary_folder=os.path.join(data_dir,label)
             print(temporary_folder)
 
-            if images:
-                print("Images detected")
-                for image in images:
-                    train_image_path=os.path.join(temporary_folder,image)
-                    train.append(cv2.imread(train_image_path))
+            scanned_images=os.listdir(temporary_folder)
+
+            if scanned_images:
+                for image in scanned_images:
+                    image_path=os.path.join(temporary_folder,image)
+                    images.append(cv2.imread(image_path))
+                    Ls.append(label)
             else:
                 print("No image detected")
     
-    for label in test_labels:
-        if os.path.isdir(os.path.join(test_dir,label)):
-            temporary_folder=os.path.join(test_dir,label)
-            images=os.listdir(temporary_folder)
-            print(temporary_folder)
-
-        if images:
-            print("Images detected")
-            for image in images:
-                test_image_path=os.path.join(temporary_folder,image)
-                test.append(cv2.imread(test_image_path))
-        else:
-            print("No image detected")
-    
-    #Processes all image inside the train list
+    #Processes all image inside the list
     i=1
-    for t in train:
-        gray_image=cv2.cvtColor(t,cv2.COLOR_BGR2GRAY)
+    for im in images:
+        gray_image=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
         face=face_classifier.detectMultiScale(gray_image)
                 
         for f in face:
             x,y,w,h=f
-            cv2.rectangle(t,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
 
-        cv2.imshow("Train",t)
+        cv2.imshow("Sample",im)
         cv2.waitKey(0)
 
-        print(f'Train Image no.{i}')
+        print(f'Image no.{i}')
         i+=1
 
-    #Processes all image inside the test list
-    i=1
-    for t in test:
-        gray_image=cv2.cvtColor(t,cv2.COLOR_BGR2GRAY)
-        face=face_classifier.detectMultiScale(gray_image)
-            
-        for f in face:
-            x,y,w,h=f
-            cv2.rectangle(t,(x,y),(x+w,y+h),(0,255,0),2)
-        
-        cv2.imshow("Test",t)
-        cv2.waitKey(0)
-        
-        print(f'Test Image no.{i}')
-        i+=1
+    '''
+    TrainLabel=np.array(TrainLabel)
+    TrainLabel=cv2.UMat(TrainLabel)
+    testL=np.array(testL)
+    face_classifier=cv2.face.LBPHFaceRecognizer_create()
+    face_classifier.train(train,TrainLabel)
+    print("Model successfully trained")
+    '''
 
 while choice!=3:
     print("Football Player Face Recognition")
@@ -84,7 +63,7 @@ while choice!=3:
     choice = int(input(">> "))
 
     if choice==1:
-        train_and_test(train_dir,test_dir)
+        train_and_test(data_dir)
         
     elif choice==2:
         print("Predict")
